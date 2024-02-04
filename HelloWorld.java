507098278e1ed1deb78a14cc607da7e6731fc7f0
cesar <bulwaif@hotmail.com>
2024-02-04 15:23:30 -0500

    Commit Helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/* import java.util.Date; // Importe la classe Date du package java.util
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Affiche "Hello, World!" suivi de la date actuelle
+        System.out.println("Hello, World!");
+        System.out.println("La date actuelle est : " + new Date());
+    }
+}
+*/
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
