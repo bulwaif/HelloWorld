c3fc9da78460de6a6aa74fa0a6abb6535dd44c52
cesar <bulwaif@hotmail.com>
2024-02-04 16:02:38 -0500

    modif1

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,6 +13,6 @@ public class HelloWorld {
 public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World version 02!");
     }
 }
